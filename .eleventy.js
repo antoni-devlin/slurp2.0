46c650364019c6184f6db16fd0ea3e16970c53d1
antoni-devlin <antoni.devlin@gmail.com>
2021-11-16 16:15:55 +0000

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,31 @@
+// Image processing with eleventy-img
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
+
+const imageShortcode = async (
+	relativeSrc,
+	alt,
+	className,
+	widths = [null, 300, 1000],
+	formats = ['jpeg', 'webp'],
+	sizes = '100vw'
+) => {
+	const { dir: imgDir } = path.parse(relativeSrc);
+	const fullSrc = path.join('src', relativeSrc);
+
+	const imageMetadata = await Image(fullSrc, {
+		widths,
+		formats,
+		outputDir: path.join('_site', imgDir),
+		urlPath: imgDir,
+	});
+};
+
+
 module.exports = config => {
+
+	// Shortcodes
+	config.addShortcode('image', imageShortcode);
 
 	// Set directories to pass through to the dist folder
 	config.addPassthroughCopy('./src/images/');
